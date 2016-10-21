7e2cd84376d52cad5afd26f555e3dde6f76a7a12
lit_phansiri <phansiri@uw.edu>
2016-10-20 18:34:54 -0700

    Initial Commit

---
+++ SimpleCalc-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalc-iOS
+//
+//  Created by Litthideth Phansiri on 10/20/16.
+//  Copyright © 2016 Lit Phansiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
