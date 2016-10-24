b27ed838bd498bd0e805e260b27b9c87fb9b9c8b
lit_phansiri <phansiri@uw.edu>
2016-10-24 13:12:19 -0700

    working simple calc before added features

--- SimpleCalc-iOS/ViewController.swift
+++ SimpleCalc-iOS/ViewController.swift
@@ -10,14 +10,107 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // output label for result
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    // internal variables
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Mod = "%"
+        case Empty = ""
+        case Clear = "Clear"
+    }
+    
+    var valOne = ""
+    var valTwo = ""
+    var tempStoredNumber = ""
+    var result = ""
+    var currentOperation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        outputLabel.text = "0"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func numberButton(sender: UIButton) {
+        tempStoredNumber += "\(sender.tag)"
+        outputLabel.text = tempStoredNumber
+    }
+    
+    @IBAction func modButton(sender: AnyObject) {
+        performOperation(operation: Operation.Mod)
+    }
+    
+    @IBAction func divideButton(sender: AnyObject) {
+        performOperation(operation: Operation.Divide)
+    }
+    
+    @IBAction func multipyButton(sender: AnyObject) {
+        performOperation(operation: Operation.Multiply)
+    }
+    
+    @IBAction func subtractButton(sender: AnyObject) {
+        performOperation(operation: Operation.Subtract)
+    }
+    
+    @IBAction func addButton(sender: AnyObject) {
+        performOperation(operation: Operation.Add)
+    }
+
+    
+    @IBAction func equalsButton(sender: AnyObject) {
+        performOperation(operation: currentOperation)
+    }
+    
+    @IBAction func clearButton(sender: AnyObject) {
+        valOne = ""
+        valTwo = ""
+        tempStoredNumber = ""
+        result = ""
+        currentOperation = Operation.Empty
+        outputLabel.text = "0"
+        
+    }
+    
+    
+    func performOperation(operation: Operation) {
+        if currentOperation != Operation.Empty { // test if current operation is not empty, that means there is a value in valOne
+            if tempStoredNumber != "" { // test if tempStoreNumber is not empty, that means there is a value in tempStoreNmber so add it to valTwo
+                valTwo = tempStoredNumber // adding to valTwo
+                tempStoredNumber = "" // clearing out tempStoredNumber
+                
+                // only happen if valOne and valTwo have values in them.
+                if currentOperation == .Multiply { // test if operation passed is Multiply
+                    result = "\(Double(valOne)! * Double(valTwo)!)"
+                } else if currentOperation == .Divide {
+                    result = "\(Double(valOne)! / Double(valTwo)!)"
+                } else if currentOperation == .Subtract {
+                    result = "\(Double(valOne)! - Double(valTwo)!)"
+                } else if currentOperation == .Add {
+                    result = "\(Double(valOne)! + Double(valTwo)!)"
+                } else if currentOperation == .Mod {
+                    result = "\(Int(valOne)! % Int(valTwo)!)"
+                }
+                
+                // results is added to valOne for current running number
+                valOne = result
+                outputLabel.text = result
+                
+            }
+        } else { // there is no number in valOne
+            valOne = tempStoredNumber
+            tempStoredNumber = ""
+            currentOperation = operation
+        }
     }
 
 
